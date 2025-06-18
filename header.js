742a4c8e93a15c4141c59a089720f2b7f72d480c
William Careri <96088123+wcareri@users.noreply.github.com>
2025-06-18 17:56:02 -0400

    Update header.js

--- header.js
+++ header.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
         <header class="fixed-header">
             <nav class="header-nav">
                 <a href="index.html"> 
-                    <img src="assets/images/header-logo.png" alt="A Sans-Serif VizDex typographic logo" class="header-logo">
+                    <img src="https://vizdexproject.com/assets/images/header-logo.png" alt="A Sans-Serif VizDex typographic logo" class="header-logo">
                 </a>
                 <div class="header-buttons">
                     <a href="info/index.html" rel="info">Info</a>
