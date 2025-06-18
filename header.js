6a899fd3dccbcf7086ade58b58065ea7d462e580
William Careri <96088123+wcareri@users.noreply.github.com>
2025-06-18 17:57:46 -0400

    Update header.js

--- header.js
+++ header.js
@@ -6,8 +6,8 @@ document.addEventListener('DOMContentLoaded', function() {
                     <img src="https://vizdexproject.com/assets/images/header-logo.png" alt="A Sans-Serif VizDex typographic logo" class="header-logo">
                 </a>
                 <div class="header-buttons">
-                    <a href="info/index.html" rel="info">Info</a>
-                    <a href="submit/index.html" rel="submit">Submit your blog</a>
+                    <a href="https://vizdexproject.com/info" rel="info">Info</a>
+                    <a href="https://vizdexproject.com/submit" rel="submit">Submit your blog</a>
                 </div>
             </nav>
         </header>
